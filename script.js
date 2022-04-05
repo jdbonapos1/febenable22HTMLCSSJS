b701b94316569385045766417ebd49f873972205
John Bonapos <johnbonapos@outlook.com>
2022-04-05 11:24:16 +0100

    js

---
+++ script.js
@@ -0,0 +1,19 @@
+// USE STRICT AND CONSOLE
+/* use strict:
+forces us to write 'secure' JavaScript
+stops us using protected words, accidentally making global variables.
+*/
+
+'use strict'
+
+console.log('JD');
+
+console.info('Bon');
+
+console.warn('UK');
+
+console.error('Cancer');
+
+let car = "Toyota";
+let model = "Corolla";
+console.info("My favourite car is a ",car,". and the model is ",model);
